64cbb70e3bf1309d625cec644d5472e511ba03f1
DL agency <dlagency@MacBook-Air-air13.local>
2020-02-09 15:19:48 +0200

    Updates

---
+++ precache-manifest.7cc2d674e034fcc8dc167eaa678cc18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0c9b7bccdd3754f85580e58f208441",
+    "url": "/goit-react-hw-01-component/index.html"
+  },
+  {
+    "revision": "7c7ffb75169eee5649ff",
+    "url": "/goit-react-hw-01-component/static/css/main.300b646f.chunk.css"
+  },
+  {
+    "revision": "37ba90ee26e2deec0342",
+    "url": "/goit-react-hw-01-component/static/js/2.719818e0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-component/static/js/2.719818e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7ffb75169eee5649ff",
+    "url": "/goit-react-hw-01-component/static/js/main.efccf376.chunk.js"
+  },
+  {
+    "revision": "6b5baa846e56ec31f022",
+    "url": "/goit-react-hw-01-component/static/js/runtime-main.4be2a2ce.js"
+  }
+]);
